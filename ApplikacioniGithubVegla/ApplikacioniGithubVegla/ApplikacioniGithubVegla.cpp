d33fc124a014eff2a4c300741cd081a6e42cf12d
alzademaj <alza.demaj@student.uni-pr.edu>
2026-01-03 16:28:46 +0100

    initial commit

--- ApplikacioniGithubVegla/ApplikacioniGithubVegla/ApplikacioniGithubVegla.cpp
+++ ApplikacioniGithubVegla/ApplikacioniGithubVegla/ApplikacioniGithubVegla.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+double f(double a, double b, double c, double x) {
+    return a * x * x + b * x + c;
+}
+
 int main()
 {
     int mbledh3numra(int a, int b, int c)
@@ -12,6 +16,9 @@ int main()
         return x + y;
     }
     cout << "Hello World!\n";
+
+    cout << "y = " << f(2.0, 1.5, 3.2, 6.1);
+    return 0;
 }
 int total=mbledh3numra(5,10,15);
 if (total>20)
@@ -21,4 +28,3 @@ if (total>20)
 } else {
     cout <<"Shuma eshte 20 ose me e vogel se 20"<<endl;
 }
-// koment
